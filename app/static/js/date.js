3d9b05e0f64ee8788ed9795d905627709e6291db
scepticsurfer <scepticsurfer@outlook.com>
2021-04-22 08:23:33 +0300

    links corrected, folders JS and PICS relocated to STATIC

---
+++ app/static/js/date.js
@@ -0,0 +1,7 @@
+function showToday() {
+ var monthNames = new Array("tammikuuta","helmikuuta","maaliskuuta","huhtikuuta","toukukuuta","kesäkuuta","heinäkuuta","elokuuta","syyskuuta","lokakuuta","marraskuuta","joulukuuta");
+ var dt = new Date();
+ var y = dt.getYear();
+ if (y < 1000) y +=1900;
+ document.write(dt.getDate() + ". " + monthNames[dt.getMonth()] + " " + y);
+}
