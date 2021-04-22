166418c3e7b6232db434fdfd7b96120dd1832faa
Liubov Pellinen <carie@mail.ru>
2021-04-22 18:11:46 +0300

    liubov-olga compare and combined

--- app/templates/js/date.js
+++
@@ -1,7 +0,0 @@
-function showToday() {
- var monthNames = new Array("tammikuuta","helmikuuta","maaliskuuta","huhtikuuta","toukukuuta","kesäkuuta","heinäkuuta","elokuuta","syyskuuta","lokakuuta","marraskuuta","joulukuuta");
- var dt = new Date();
- var y = dt.getYear();
- if (y < 1000) y +=1900;
- document.write(dt.getDate() + ". " + monthNames[dt.getMonth()] + " " + y);
-}
