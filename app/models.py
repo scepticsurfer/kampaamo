f8b81275f75611ccc67103715b4e0e18224ac453
scepticsurfer <scepticsurfer@outlook.com>
2021-04-22 17:17:59 +0300

    models.py changed (boolean)

--- app/models.py
+++ app/models.py
@@ -22,8 +22,10 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     # role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
-    admin=db.Column(db.Enum)
-    hairdresser=db.Column(db.Enum)
+    #admin=db.Column(db.Enum)
+    #hairdresser=db.Column(db.Enum)
+    admin = db.Column(db.Boolean, default=False)
+    hairdresser = db.Column(db.Boolean, default=False)
     confirmed = db.Column(db.Boolean, default=False)
 
     @property
